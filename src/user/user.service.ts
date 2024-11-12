b253b04e81617391177dd0de6b02929d71fa99ab
vlad mungin <vlad.mungin@yandex.ru>
2024-11-12 16:40:28 +0300

    fix auth

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -29,7 +29,7 @@ export class UserService {
   async create(dto: AuthDto) {
     const user = {
       email: dto.email,
-      name: '',
+      name: dto.name,
       password: await hash(dto.password),
       role: 'admin',
     }

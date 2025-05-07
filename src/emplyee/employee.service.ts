86e0a19d0a6b2765ebe31b507bf5e4a381f77957
gleb <glen2004111@gmail.com>
2025-05-07 19:35:17 +0300

    svae

--- src/emplyee/employee.service.ts
+++ src/emplyee/employee.service.ts
@@ -36,7 +36,7 @@ export class EmployeeService {
     if (!specialization) {
       currentSpecialization = await this.specialization.create({
         name: createEmployeeDto.specialization,
-        userId: '',
+        userId: userId,
       })
     } else {
       currentSpecialization = specialization
